79595d70d99e2d7cb19b34325cc3ed583a812c29
Michael Bushoy <mbushoy@gmail.com>
2016-02-02 16:32:48 -0500

    colors

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
-var express = require('express'); 
-var morgan = require('morgan'); 
-var bodyParser = require('body-parser'); 
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
 var swig = require('swig');
 var path = require('path');
+var dotenv = require('dotenv').config();
 
-var app = express(); 
+var app = express();
 
-var port = 3000; 
-
-
-// swig set up 
-
-app.set('views', __dirname + '/views'); 
-app.set('view engine', 'html'); 
-app.engine('html', swig.renderFile); 
+var port = 3000;
 
 
-//logging and bodyparsing 
+// swig set up
 
-app.use(morgan('dev')); 
-app.use(bodyParser.json()); 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.engine('html', swig.renderFile);
+
+
+//logging and bodyparsing
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 
-// routing static files 
+// routing static files
+app.use('/public',express.static(path.join(__dirname, '/public')));
 app.use('/bootstrap', express.static(path.join(__dirname, './node_modules/bootstrap/dist')));
 app.use('/jquery', express.static(path.join(__dirname, './node_modules/jquery/dist')));
 
-// 404 not found error 
 
-
+// 404 not found error
 app.use(require('./routes'));
 
-// error handling 
+// error handling
 app.use(function(err, req, res, next){
-  res.status(err.status || 500); 
-  // render some error html 
+  res.status(err.status || 500);
+  // render some error html
 })
 
 app.listen(port, function(){
