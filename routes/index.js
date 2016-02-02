cbcbad6ef90706de63d785a8cff7a1d42ed27356
apackin <apackin@gmail.com>
2016-02-02 13:31:28 -0500

    set up framework

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express')
+var router = express.Router(); 
+var Bluebird = require('bluebird')
+var models = require('../models'); 
+var Hotel = models.Hotel; 
+var Restaurant = models.Restaurant; 
+var Activity = models.Activity; 
+
+
+router.get('/', function(req, res, next){
+  Bluebird.all([
+    Hotel.find({}),
+    Restaurant.find({}), 
+    Activity.find({})
+  ])
+  .spread(function(hotels, restaurants, activities){
+    // render some index html that doesnt exist yet with this data 
+    // res.send({hotels:hotels, restaurants:restaurants, activities:activities});
+    res.render("index", {hotels:hotels, restaurants:restaurants, activities:activities});
+  });
+ 
+
+});
+
+
+module.exports = router; 
